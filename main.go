a1d7e71777b1288a5fb1154647fa24b1ca2f170d
yash kumar <yashsaini1998220@gmail.com>
2024-07-17 18:21:37 +0530

    ok

--- main.go
+++ main.go
@@ -1,27 +1,54 @@
-// API
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/gorilla/mux"
-	"net/http"
-)
+	"time"
 
-const (
-	WEBPORT = ":8080"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	uri := "mongodb://localhost:27017"
 
-	router := mux.NewRouter()
+	clientOptions := options.Client().ApplyURI(uri)
 
-	http.Handle("/", router)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-	router.HandleFunc("/home", homefunc)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		fmt.Println("Error connecting", err)
+	}
 
-	http.ListenAndServe(WEBPORT, nil)
-}
+	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
 
-func homefunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to the home page")
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Println("Ping failed", err)
+	}
+
+	fmt.Println("Connected successfully")
+
+	collection := client.Database("work").Collection("work")
+
+	doc := bson.D{{"name", "rahul"}, {"age", 25}}
+
+	insertResult, err := collection.InsertOne(context.TODO(), doc)
+	if err != nil {
+		fmt.Println("Error inserting", err)
+
+	}
+	fmt.Println("insert succeeded", insertResult)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Disconnect(ctx)
+
+	fmt.Println("Disconnected successfully")
+
 }
